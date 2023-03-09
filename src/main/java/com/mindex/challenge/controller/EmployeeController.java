d3c0c312143b74ca42677c0d51694b000b7bd328
stynan27 <seamustynan16@gmail.com>
2023-03-09 18:37:13 -0500

    Modified employee create logging output

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.mindex.challenge.controller;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,7 @@ public class EmployeeController {
 
     @PostMapping("/employee")
     public Employee create(@RequestBody Employee employee) {
-        LOG.debug("Received employee create request for [{}]", employee);
+        LOG.debug("Received employee create request for [{}]", employee.getFirstName());
 
         return employeeService.create(employee);
     }
